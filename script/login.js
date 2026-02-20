af1d082e010b044830104786508b53042ab2c18b
shuvr0shishir <shuvroshishirx@gmail.com>
2026-02-20 12:05:06 +0600

    login functionality added

--- script/login.js
+++ script/login.js
@@ -1,1 +1,21 @@
-console.log("Login Connected");
+// login functionality
+
+const loginBtn = document.getElementById('login-btn');
+loginBtn.addEventListener('click', () => {
+    const numberInput = document.getElementById('number-input');
+    const pinInput = document.getElementById('pin-input');
+    if (numberInput.value === '' && numberInput.value === '') {
+        alert('Input Number And Password.');
+    } else if (numberInput.value == '01234567890' && pinInput.value == '1234') {
+        window.location.assign('./home.html');
+        alert('Login Successful...');
+    } else {
+        if (numberInput.value != '01234567890') {
+            alert('Login Failed! NUMBER is not Registered.');
+        } else if (pinInput.value != '1234') {
+            alert('Login Failed! PIN Wrong');
+        }
+    }
+    numberInput.value = '';
+    pinInput.value = '';
+})
